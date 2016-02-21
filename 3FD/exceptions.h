87910cb4e7ca834e031adc2a86192486d10fcae1
Felipe Aburaya <felipe.aburaya@hotmail.com>
2016-02-21 11:55:09 -0300

    added fix for compilation error that does not happen every time

--- 3FD/exceptions.h
+++ 3FD/exceptions.h
@@ -8,6 +8,11 @@
 #include <memory>
 #include <future>
 
+#ifdef _WIN32
+#	define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#   include <windows.h>
+#endif
+
 namespace _3fd
 {
 	using std::string;
