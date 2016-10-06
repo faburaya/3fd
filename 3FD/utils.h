b9acb129496867aa3a2aa1dfb2a38b3ddb111356
Felipe Aburaya <felipe.aburaya@hotmail.com>
2016-10-05 23:48:04 -0300

    fixed compilation error in linux caused by lack of preprocessor condition to compile shared_mutex (which dependes on SRWLOCK) only in Windows platform

--- 3FD/utils.h
+++ 3FD/utils.h
@@ -34,78 +34,77 @@ namespace _3fd
 #ifdef _MSC_VER
 		_ALLOCATOR_DECL(CACHE_CHUNKLIST, stdext::allocators::sync_none, unsafe_pool_allocator);
 #endif
+	    /// <summary>
+	    /// Provides uninitialized and contiguous memory.
+	    /// There is a limit 4 GB, which is far more than enough.
+	    /// The pool was designed for single-thread access.
+	    /// </summary>
+	    class MemoryPool : notcopiable
+	    {
+	    private:
 
-		/// <summary>
-		/// Provides uninitialized and contiguous memory.
-		/// There is a limit 4 GB, which is far more than enough.
-		/// The pool was designed for single-thread access.
-		/// </summary>
-		class MemoryPool : notcopiable
-		{
-		private:
+		    void	*m_baseAddr,
+				    *m_nextAddr,
+				    *m_end;
 
-			void	*m_baseAddr,
-					*m_nextAddr,
-					*m_end;
+		    const uint32_t m_blockSize;
 
-			const uint32_t m_blockSize;
+		    /// <summary>
+		    /// Keeps available memory blocks as offset integers to the base address.
+		    /// Because the offset is a 32 bit integer, this imposes the practical limit of 4 GB to the pool.
+		    /// </summary>
+		    std::stack<uint32_t> m_availableAddrsAsOffset;
 
-			/// <summary>
-			/// Keeps available memory blocks as offset integers to the base address.
-			/// Because the offset is a 32 bit integer, this imposes the practical limit of 4 GB to the pool.
-			/// </summary>
-			std::stack<uint32_t> m_availableAddrsAsOffset;
+	    public:
 
-		public:
+		    MemoryPool(uint32_t numBlocks, uint32_t blockSize);
 
-			MemoryPool(uint32_t numBlocks, uint32_t blockSize);
+		    MemoryPool(MemoryPool &&ob);
 
-			MemoryPool(MemoryPool &&ob);
+		    ~MemoryPool();
 
-			~MemoryPool();
+		    size_t GetNumBlocks() const throw();
 
-			size_t GetNumBlocks() const throw();
+		    void *GetBaseAddress() const throw();
 
-			void *GetBaseAddress() const throw();
+		    bool Contains(void *addr) const throw();
 
-			bool Contains(void *addr) const throw();
+		    bool IsFull() const throw();
 
-			bool IsFull() const throw();
+		    bool IsEmpty() const throw();
 
-			bool IsEmpty() const throw();
+		    void *GetFreeBlock() throw();
 
-			void *GetFreeBlock() throw();
+		    void ReturnBlock(void *addr);
+	    };
 
-			void ReturnBlock(void *addr);
-		};
+	    /// <summary>
+	    /// A template class for a memory pool that expands dynamically.
+	    /// The pool was designed for single-thread access.
+	    /// </summary>
+	    class DynamicMemPool : notcopiable
+	    {
+	    private:
 
-		/// <summary>
-		/// A template class for a memory pool that expands dynamically.
-		/// The pool was designed for single-thread access.
-		/// </summary>
-		class DynamicMemPool : notcopiable
-		{
-		private:
+		    const float		m_increasingFactor;
+		    const size_t	m_initialSize,
+						    m_blockSize;
 
-			const float		m_increasingFactor;
-			const size_t	m_initialSize,
-							m_blockSize;
+		    std::map<void *, MemoryPool>	m_memPools;
+		    std::queue<MemoryPool *>		m_availableMemPools;
 
-			std::map<void *, MemoryPool>	m_memPools;
-			std::queue<MemoryPool *>		m_availableMemPools;
+	    public:
 
-		public:
+		    DynamicMemPool(size_t initialSize,
+			    size_t blockSize,
+			    float increasingFactor);
 
-			DynamicMemPool(size_t initialSize,
-				size_t blockSize,
-				float increasingFactor);
+		    void *GetFreeBlock();
 
-			void *GetFreeBlock();
+		    void ReturnBlock(void *object);
 
-			void ReturnBlock(void *object);
-
-			void Shrink();
-		};
+		    void Shrink();
+	    };
 
         ////////////////////////////////////////////////
         // Multi-thread and Synchronization Utilities
@@ -147,6 +146,7 @@ namespace _3fd
             static void InvokeAndLeave(const std::function<void()> &callback);
         };
 
+#   ifdef _WIN32
         /// <summary>
         /// Alternative implementation for std::shared_mutex from C++14 Std library.
         /// </summary>
@@ -170,7 +170,7 @@ namespace _3fd
             void lock();
             void unlock();
         };
-
+#   endif
 	}// end of namespace utils
 }// end of namespace _3fd
 
