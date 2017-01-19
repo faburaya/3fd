d8801760584767d13a773a172db88143276948f3
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2017-01-18 22:44:00 -0200

    started implementing broker module: high level design of classes

---
+++ 3FD/broker.h
@@ -0,0 +1,68 @@
+#ifndef BROKER_H // header guard
+#define BROKER_H
+
+#include "base.h"
+#include <string>
+#include <cinttypes>
+#include <Poco/Data/Session.h>
+#include <Poco/Data/Statement.h>
+
+namespace _3fd
+{
+namespace broker
+{
+    using std::string;
+
+    /// <summary>
+    /// Enumerates options for service broker backend.
+    /// </summary>
+    enum class Backend : short
+    {
+        MsSqlServer,
+        OracleDatabase
+    };
+
+    /// <summary>
+    /// Enumerates options for backend validation of message content.
+    /// </summary>
+    enum class MessageContentValidation : short
+    {
+        None,
+        WellFormedXml
+    };
+
+    /// <summary>
+    /// Packs the necessary info to specify a message type for creation or use.
+    /// </summary>
+    struct MessageTypeSpec
+    {
+        string url;
+        uint32_t nBytes;
+        MessageContentValidation contentValidation;
+    };
+
+    /// <summary>
+    /// Represents a queue in the broker, from which
+    /// a service can read its incoming messages.
+    /// </summary>
+    class QueueReader
+    {
+    private:
+
+        Poco::Data::Session m_dbSession;
+        Poco::Data::Statement m_readMsgStmt;
+
+    public:
+
+        QueueReader(Backend svcBrokerBackend,
+                    const string &connString,
+                    const string &serviceURL,
+                    uint8_t queueId,
+                    const MessageTypeSpec &msgTypeSpec);
+
+        ;
+    };
+}
+}// end of namespace _3fd
+
+#endif // end of header guard
