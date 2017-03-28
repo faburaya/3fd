4a79ed014f610393c53f74fc4715865dc60177e7
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2017-03-28 07:58:10 -0300

    further impl reading XML config

--- ImageTranscoder/MetadataCopier.cpp
+++ ImageTranscoder/MetadataCopier.cpp
@@ -4,10 +4,11 @@
 #include "3FD\exceptions.h"
 #include <MsXml6.h>
 #include <map>
+#include <set>
 #include <array>
 #include <sstream>
 #include <codecvt>
-
+#include <algorithm>
 
 /* XML parsing validates the content against the referenced schema (XSD), thus the calls for
    browsing the DOM are not supposed to fail. Their results are only checked because failures
@@ -18,6 +19,20 @@
 namespace application
 {
     using namespace _3fd::core;
+
+    /// <summary>
+    /// Extracts the BSTR from a COM wrapped VARIANT, bypassing its deallocation.
+    /// </summary>
+    /// <param name="wrappedVar">The wrapped VARIANT.</param>
+    /// <returns>The extracted BSTR;</returns>
+    static BSTR ExtractBstrFrom(CComVariant &wrappedVar)
+    {
+        _ASSERTE(wrappedVar.vt == VT_BSTR);
+        VARIANT rawVar;
+        VariantInit(&rawVar);
+        wrappedVar.Detach(&rawVar);
+        return rawVar.bstrVal;
+    }
 
     /// <summary>
     /// Hashes the unique identifier using FNV1a algorithm.
@@ -111,18 +126,22 @@ namespace application
             CComVariant formatNameAsVariant;
             CComPtr<IXMLDOMNode> attrNode;
             CHECK(attributes->getNamedItem(attrNameFormats[idx], &attrNode));
-            CHECK(elemNode->get_nodeValue(&formatNameAsVariant));
+            CHECK(attrNode->get_nodeValue(&formatNameAsVariant));
             _ASSERTE(formatNameAsVariant.vt == VT_BSTR);
 
             auto iter = containerFormatByName.find(
                 HashName(formatNameAsVariant.bstrVal)
             );
 
+            // does the format have a GUID?
             if (containerFormatByName.end() == iter)
             {
+                CComBSTR xmlSource;
+                elemNode->get_xml(&xmlSource);
+
                 std::wostringstream woss;
-                woss << L"Invalid metadata map case in configuration file: "
-                        L"Microsoft WIC GUID was not defined for container format " << formatNameAsVariant.bstrVal;
+                woss << L"Invalid setting in configuration file! Microsoft WIC GUID was not defined for container format "
+                     << formatNameAsVariant.bstrVal << L". Ocurred in:\r\n" << xmlSource;
 
                 std::wstring_convert<std::codecvt_utf8<wchar_t>> strConv;
                 throw AppException<std::runtime_error>(strConv.to_bytes(woss.str()));
@@ -135,77 +154,200 @@ namespace application
     }
 
     /// <summary>
+    /// Represents an entry in the list of metadata map cases.
+    /// </summary>
+    struct MapCaseEntry
+    {
+        uint64_t key;
+        BSTR fromPath;
+        BSTR toPath;
+        uint32_t metaFmtNameHash;
+        bool onlyCommon;
+    };
+
+    typedef std::vector<MapCaseEntry> VecOfCaseEntries;
+
+    /// <summary>
+    /// Binary search in sub-range of vector containing map cases entries.
+    /// </summary>
+    /// <param name="searchKey">The key to search for.</param>
+    /// <param name="begin">An iterator to the first position of the sub-range.
+    /// In the end, this parameter keeps the beginning of the last sub-range this
+    /// iterative algorithm has delved into.</param>
+    /// <param name="begin">An iterator to one past the last position of the sub-range.
+    /// In the end, this parameter keeps the end of the last sub-range this
+    /// iterative algorithm has delved into.</param>
+    /// <returns>An iterator to the found entry. Naturally, it cannot refer to
+    /// the same position in the parameter 'end', unless there was no match.</returns>
+    VecOfCaseEntries::const_iterator BinarySearch(uint64_t searchKey,
+                                                  VecOfCaseEntries::const_iterator &begin,
+                                                  VecOfCaseEntries::const_iterator &end)
+    {
+        auto endOfRange = end;
+
+        while (begin != end)
+        {
+            auto middle = begin + std::distance(begin, end) / 2;
+
+            if (middle->key < searchKey)
+                begin = middle + 1;
+            else if (middle->key > searchKey)
+                end = middle;
+            else
+                return middle;
+        }
+
+        return endOfRange;
+    }
+
+    /// <summary>
     /// Holds the metadata map cases loaded from configuration file.
     /// </summary>
     class MetadataMapCases
     {
     private:
 
-        struct MapCaseEntry
-        {
-            uint64_t key;
-            BSTR fromPath;
-            BSTR toPath;
-            uint32_t metaFmtNameHash;
-            bool copyAllItems;
-        };
-
-        std::vector<MapCaseEntry> m_mapCases;
+        VecOfCaseEntries m_mapCasesEntries;
 
     public:
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MetadataMapCases" /> class.
+        /// </summary>
+        /// <param name="dom">The DOM parsed from the XML configuration file.</param>
+        /// <param name="mapCasesXPath">The XPath to the map cases.</param>
+        /// <param name="containerFormatByName">A dictionary of container formats ordered by the hash of their names.</param>
+        /// <param name="metadataFormatByName">A dictionary of metadata formats ordered by the hash of their names.</param>
         MetadataMapCases(IXMLDOMDocument2 *dom,
                          BSTR mapCasesXPath,
-                         const Hash2HashMap &containerFormatByName)
+                         const Hash2HashMap &containerFormatByName,
+                         const Hash2HashMap &metadataFormatByName)
         {
             CALL_STACK_TRACE;
 
             try
             {
                 HRESULT hr;
+                std::set<decltype(MapCaseEntry::key)> uniqueKeys;
+
+                const CComBSTR attrNameMetaFormat(L"metaFormat"),
+                               attrNamePathFrom(L"fromPath"),
+                               attrNamePathTo(L"toPath"),
+                               attrNameOnlyCommon(L"onlyCommon");
+
                 CComPtr<IXMLDOMNodeList> listOfMapCaseNodes;
                 CHECK(dom->selectNodes(mapCasesXPath, &listOfMapCaseNodes));
 
-                // Iterate over list of map cases:
-
                 long mapCaseNodesCount;
                 CHECK(listOfMapCaseNodes->get_length(&mapCaseNodesCount));
+
+                std::vector<MapCaseEntry> mapCasesEntries;
+                mapCasesEntries.reserve(mapCaseNodesCount);
+
+                // Iterate over list of map cases:
+
                 for (long idxCase = 0; idxCase < mapCaseNodesCount; ++idxCase)
                 {
                     CComPtr<IXMLDOMNode> mapCaseNode;
                     CHECK(listOfMapCaseNodes->get_item(idxCase, &mapCaseNode));
 
                     auto mapCaseKey = GetMetadataMapCaseKey(mapCaseNode, containerFormatByName);
-
-                    CComPtr<IXMLDOMNodeList> listOfEntryNodes;
-                    CHECK(mapCaseNode->get_childNodes(&listOfEntryNodes));
+                    
+                    // is map case unique?
+                    if (!uniqueKeys.insert(mapCaseKey).second)
+                    {
+                        std::wstring_convert<std::codecvt_utf8<wchar_t>> strConv;
+                        std::wostringstream woss;
+                        CComBSTR xmlSource;
+                        mapCaseNode->get_xml(&xmlSource);
+                        woss << L"Configuration file cannot have duplicated metadata map cases! Ocurred in:\r\n" << xmlSource;
+                        throw AppException<std::runtime_error>(strConv.to_bytes(woss.str()));
+                    }
 
                     // Iterate over list entries:
 
                     long entryNodesCount;
+                    CComPtr<IXMLDOMNodeList> listOfEntryNodes;
+                    CHECK(mapCaseNode->get_childNodes(&listOfEntryNodes));
                     CHECK(listOfEntryNodes->get_length(&entryNodesCount));
+
                     for (long idxEntry = 0; idxEntry < entryNodesCount; ++idxEntry)
                     {
+                        mapCasesEntries.emplace_back();
+                        auto &newEntry = mapCasesEntries.back();
+                        newEntry.key = mapCaseKey;
+
                         CComPtr<IXMLDOMNode> entryNode;
                         CHECK(listOfEntryNodes->get_item(idxEntry, &entryNode));
-
-                        const CComBSTR attrNameMetaFormat(L"metaFormat"),
-                                       attrNamePathFrom(L"fromPath"),
-                                       attrNamePathTo(L"toPath"),
-                                       attrNameCopyAllItems(L"copyAllItems");
 
                         CComPtr<IXMLDOMNamedNodeMap> attributes;
                         CHECK(entryNode->get_attributes(&attributes));
 
-                        // TO DO
+                        // get metadata format:
+
+                        CComVariant metaFormatNameAsVar;
+                        CComPtr<IXMLDOMNode> metaFormatAttrNode;
+                        CHECK(attributes->getNamedItem(attrNameMetaFormat, &metaFormatAttrNode));
+                        CHECK(metaFormatAttrNode->get_nodeValue(&metaFormatNameAsVar));
+                        newEntry.metaFmtNameHash = HashName(metaFormatNameAsVar.bstrVal);
+
+                        // does the format have a GUID?
+                        if (metadataFormatByName.find(newEntry.metaFmtNameHash) == metadataFormatByName.end())
+                        {
+                            CComBSTR xmlSource;
+                            mapCaseNode->get_xml(&xmlSource);
+
+                            std::wostringstream woss;
+                            woss << L"Invalid setting in configuration file! Microsoft WIC GUID was not defined for metadata format "
+                                 << metaFormatNameAsVar.bstrVal << L". Ocurred in:\r\n" << xmlSource;
+
+                            std::wstring_convert<std::codecvt_utf8<wchar_t>> strConv;
+                            throw AppException<std::runtime_error>(strConv.to_bytes(woss.str()));
+                        }
+
+                        // get metadata source path:
+
+                        CComVariant fromPathAsVar;
+                        CComPtr<IXMLDOMNode> fromPathAttrNode;
+                        CHECK(attributes->getNamedItem(attrNamePathFrom, &fromPathAttrNode));
+                        CHECK(fromPathAttrNode->get_nodeValue(&fromPathAsVar));
+                        newEntry.fromPath = ExtractBstrFrom(fromPathAsVar);
+
+                        // get metadata destination path:
+
+                        CComVariant toPathAsVar;
+                        CComPtr<IXMLDOMNode> toPathAttrNode;
+                        CHECK(attributes->getNamedItem(attrNamePathTo, &toPathAttrNode));
+                        CHECK(toPathAttrNode->get_nodeValue(&toPathAsVar));
+                        newEntry.toPath = ExtractBstrFrom(toPathAsVar);
+
+                        // copy only common items?
+
+                        CComVariant onlyCommonAsVar;
+                        CComPtr<IXMLDOMNode> onlyCommonAttrNode;
+                        CHECK(attributes->getNamedItem(attrNameOnlyCommon, &onlyCommonAttrNode));
+                        CHECK(onlyCommonAttrNode->get_nodeValue(&onlyCommonAsVar));
+                        _ASSERTE(onlyCommonAsVar.vt == VT_BOOL);
+                        newEntry.onlyCommon = (onlyCommonAsVar.boolVal == VARIANT_TRUE);
 
                     }// end of inner for loop
 
                 }// end of outer for loop
+
+                // sort the entries by key:
+                std::sort(mapCasesEntries.begin(), mapCasesEntries.end(),
+                    [](const MapCaseEntry &left, const MapCaseEntry &right) { return left.key < right.key; }
+                );
+
+                m_mapCasesEntries.swap(mapCasesEntries);
             }
             catch (IAppException &)
             {
                 throw; // just forward exceptions for errors known to have been already handled
+            }
+            catch (CAtlException &ex)
+            {
+                WWAPI::RaiseHResultException(ex, "Failed to read metadata map cases from configuration", "COM ATL");
             }
             catch (std::exception &ex)
             {
@@ -213,6 +355,69 @@ namespace application
                 oss << "Generic failure when reading metadata map cases from configuration: " << ex.what();
                 throw AppException<std::runtime_error>(oss.str());
             }
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="MetadataMapCases"/> class.
+        /// </summary>
+        ~MetadataMapCases()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the sub range of entries that match the given key.
+        /// </summary>
+        /// <param name="searchKey">The key to search.</param>
+        /// <param name="subRangeBegin">An iterator to the first position of the sub-range.</param>
+        /// <param name="subRangeEnd">An iterator to one past the last position of the sub-range.</param>
+        /// <returns>When a sub-range has been found, <c>true</c>, otherwise, <c>false</c>.</returns>
+        bool GetSubRange(uint64_t searchKey,
+                         VecOfCaseEntries::const_iterator &subRangeBegin,
+                         VecOfCaseEntries::const_iterator &subRangeEnd) const
+        {
+            struct {
+                VecOfCaseEntries::const_iterator begin;
+                VecOfCaseEntries::const_iterator end;
+            } firstMatchRightPart;
+
+            auto begin = m_mapCasesEntries.begin();
+            auto end = m_mapCasesEntries.end();
+            auto match = BinarySearch(searchKey, begin, end);
+
+            // match? this is the first, so keep the partition at the right:
+            if (match != end)
+            {
+                firstMatchRightPart.begin = match; // match inclusive
+                firstMatchRightPart.end = end;
+            }
+            else
+            {
+                subRangeBegin = subRangeEnd = m_mapCasesEntries.end();
+                return false; // no match found!
+            }
+
+            do
+            {
+                subRangeBegin = end = match; // last match is the smallest entry found!
+                match = BinarySearch(searchKey, begin, end); // continue to search in the left partition
+            }
+            while (match != end);
+
+            /* Now go back to the partition at the right of the first
+               match, and start looking for the end of the range: */
+
+            match = firstMatchRightPart.begin;
+            end = firstMatchRightPart.end;
+
+            do
+            {
+                subRangeEnd = begin = match + 1; // last match is the greatest entry found!
+                match = BinarySearch(searchKey, begin, end); // continue to search in the right partition
+            }
+            while (match != end);
+
+            return true;
         }
 
     };// end of MetadataMapCases class
@@ -261,9 +466,9 @@ namespace application
 
                 GUID guid;
                 CComVariant guidAsVariant;
-                node.Release();
-                CHECK(attributes->getNamedItem(attrNameGuid, &node));
-                CHECK(node->get_nodeValue(&guidAsVariant));
+                CComPtr<IXMLDOMNode> guidAttrNode;
+                CHECK(attributes->getNamedItem(attrNameGuid, &guidAttrNode));
+                CHECK(guidAttrNode->get_nodeValue(&guidAsVariant));
                 _ASSERTE(guidAsVariant.vt == VT_BSTR);
                 CHECK(IIDFromString(guidAsVariant.bstrVal, &guid));
                 uint32_t hashedGuid = HashGuid(guid);
@@ -271,31 +476,33 @@ namespace application
                 // get name:
 
                 CComVariant nameAsVariant;
-                node.Release();
-                CHECK(attributes->getNamedItem(attrNameName, &node));
-                CHECK(node->get_nodeValue(&nameAsVariant));
+                CComPtr<IXMLDOMNode> nameAttrNode;
+                CHECK(attributes->getNamedItem(attrNameName, &nameAttrNode));
+                CHECK(nameAttrNode->get_nodeValue(&nameAsVariant));
                 _ASSERTE(nameAsVariant.vt == VT_BSTR);
                 uint32_t hashedName = HashName(nameAsVariant.bstrVal);
 
                 // check whether GUID is unique in list:
                 if (!nameByGuid.emplace(hashedGuid, hashedName).second)
                 {
-                    std::wostringstream woss;
-                    woss << L"Configuration file cannot have duplicated GUID in list of formats: "
-                            L"ocurred in GUID = " << guidAsVariant.bstrVal << L", format = " << nameAsVariant.bstrVal;
+                    CComBSTR xmlSource;
+                    node->get_xml(&xmlSource);
 
                     std::wstring_convert<std::codecvt_utf8<wchar_t>> strConv;
+                    std::wostringstream woss;
+                    woss << L"Configuration file cannot have duplicated GUID in list of formats: ocurred in " << xmlSource;
                     throw AppException<std::runtime_error>(strConv.to_bytes(woss.str()));
                 }
 
                 // check whether name is unique in list:
                 if (!guidByName.emplace(hashedName, hashedGuid).second)
                 {
-                    std::wostringstream woss;
-                    woss << L"Configuration file cannot have duplicated name in list of formats: "
-                            L"ocurred in format = " << nameAsVariant.bstrVal << L", GUID = " << guidAsVariant.bstrVal;
-
+                    CComBSTR xmlSource;
+                    node->get_xml(&xmlSource);
+                    
                     std::wstring_convert<std::codecvt_utf8<wchar_t>> strConv;
+                    std::wostringstream woss;
+                    woss << L"Configuration file cannot have duplicated name in list of formats: ocurred in " << xmlSource;
                     throw AppException<std::runtime_error>(strConv.to_bytes(woss.str()));
                 }
             }
@@ -303,6 +510,10 @@ namespace application
         catch (IAppException &)
         {
             throw; // just forward exceptions for errors known to have been already handled
+        }
+        catch (CAtlException &ex)
+        {
+            WWAPI::RaiseHResultException(ex, "Failed to read formats from configuration", "COM ATL");
         }
         catch (std::exception &ex)
         {
@@ -375,6 +586,10 @@ namespace application
         catch (IAppException &)
         {
             throw; // just forward exceptions for errors known to have been already handled
+        }
+        catch (CAtlException &ex)
+        {
+            WWAPI::RaiseHResultException(ex, "Failed when loading configuration", "COM ATL");
         }
         catch (std::exception &ex)
         {
